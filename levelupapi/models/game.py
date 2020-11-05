2218f10e7940bfe12b8a2a31f2f696c2602328d3
Johnson4996 <Johnson.4996@gmail.com>
2020-11-05 15:34:56 -0600

    user is able to create new game from client side

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Game(models.Model):
     """Game database model"""
-    gamer = models.ForeignKey("Game", on_delete=models.CASCADE)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
     gametype = models.ForeignKey("GameType", on_delete=models.CASCADE)
     title = models.CharField(max_length=75)
